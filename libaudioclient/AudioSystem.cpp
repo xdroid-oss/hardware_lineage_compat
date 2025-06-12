cc39b9710e0845b71a54332103a51ea4fa59d944
ralph950412 <ralph950412@gmail.com>
2025-06-12 20:52:47 +0800

    libaudioclient: Address fixes for 25Q2
    Change-Id: I42a978595c2a6b18e309f6a8b80fbac45afc5e44

--- libaudioclient/AudioSystem.cpp
+++ libaudioclient/AudioSystem.cpp
@@ -1,19 +1,22 @@
-/*
- * Copyright (C) The LineageOS Project
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-#include "AudioSystem.h"
+#include <media/AudioSystem.h>
+#include <string>
+#include <utils/Errors.h>
+#include <vector>
 
 namespace android {
-namespace AudioSystem {
+extern "C" {
+status_t _ZN7android11AudioSystem24setDeviceConnectionStateE24audio_policy_dev_state_tRKNS_5media5audio6common9AudioPortE14audio_format_tb(
+    audio_policy_dev_state_t state,
+    const android::media::audio::common::AudioPort& port,
+    audio_format_t encodedFormat, bool deviceSwitch);
 
-status_t setDeviceConnectionState(audio_policy_dev_state_t state,
-                                  const android::media::audio::common::AudioPort& port,
-                                  audio_format_t encodedFormat) {
-    return setDeviceConnectionState(state, port, encodedFormat, false);
+status_t _ZN7android11AudioSystem24setDeviceConnectionStateE24audio_policy_dev_state_tRKNS_5media5audio6common9AudioPortE14audio_format_t(
+    audio_policy_dev_state_t state,
+    const android::media::audio::common::AudioPort& port,
+    audio_format_t encodedFormat, bool deviceSwitch
+) {
+    return _ZN7android11AudioSystem24setDeviceConnectionStateE24audio_policy_dev_state_tRKNS_5media5audio6common9AudioPortE14audio_format_tb(
+        state, port, encodedFormat, deviceSwitch);
 }
-
-}  // namespace AudioSystem
+}
 }  // namespace android
